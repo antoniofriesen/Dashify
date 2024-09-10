abf9840d3158489bc56dc9d03b5560703ace3a1e
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-09-10 07:53:56 +0200

    app posts
    App posts (blog) erfolgreich erstellt. .gitignore und auch requirements.txt auch hinzugefügt

--- dashify/dashify/urls.py
+++ dashify/dashify/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("dashboard/", views.dashboard, name="dashboad"),
     path("users/", include("users.urls")),
-    path("register/", include("users.urls")),
-    path("logout/", include("users.urls")),
+    path("posts/", include("posts.urls")),
     path("plot/", include("plot.urls"))
 ]
